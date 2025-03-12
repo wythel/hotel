6d64a10035d521ecd077aff2a8f27b4eb1b4b9dd
clin <clin@splunk.com>
2025-03-11 21:09:56 -0700

    commit

--- hotel.py
+++ hotel.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser
 import time
+import json
 from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 def parse_options():
@@ -21,16 +23,32 @@ def main():
     options = parse_options()
     driver = Chrome()
     driver.get(f"https://www.google.com/travel/search?q={options.name}&hl=zh-Hant-CA")
-
-    checkin = driver.find_element(
-        By.CSS_SELECTOR, '[placeholder="登機報到頁面"]')
-    checkin.send_keys(Keys.BACK_SPACE * len(checkin.get_attribute("value")) + options.check_in)
+    # driver.find_element(By.CSS_SELECTOR, '[aria-label="價格"]').click()
+    # time.sleep(10)
+    checkin = driver.find_element(By.CSS_SELECTOR, '[placeholder="登機報到頁面"]')
+    checkin.send_keys(
+        Keys.BACK_SPACE * len(checkin.get_attribute("value")) +
+        options.check_in + Keys.ENTER)
+    time.sleep(5)
     checkout = driver.find_element(By.CSS_SELECTOR, '[placeholder="退房"]')
-    checkout.send_keys(Keys.BACK_SPACE * len(checkout.get_attribute("value")) + options.check_out + Keys.ENTER)
-    time.sleep(10)
-    all_options = driver.find_element(By.CSS_SELECTOR, '[jsname="Z186"]')
-    elements = all_options.find_elements(By.CLASS_NAME, 'iqYCVb')
-    print([elem.text for elem in elements])
+    checkout.send_keys(
+        Keys.BACK_SPACE * len(checkout.get_attribute("value")) +
+        options.check_out + Keys.ENTER)
+    time.sleep(5)
+    # 查看更多選項
+    driver.find_element(By.CSS_SELECTOR, '[jsname="wQivvd"]').click()
+    time.sleep(5)
+
+    all_options = driver.find_elements(By.CSS_SELECTOR, '[jsname="Z186"]')[-1]
+    prices = {}
+    for row in all_options.find_elements(By.CSS_SELECTOR, '.ADs2Tc'):
+        try:
+            ota = row.find_element(By.CSS_SELECTOR, '[data-click-type="268"]').text
+            price = row.find_element(By.CLASS_NAME, 'iqYCVb').text
+            prices[ota] = price
+        except NoSuchElementException:
+            print(row.text)
+    print(prices)
 
 
 if __name__ == "__main__":
