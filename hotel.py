83d5e46c1412118a4a9cdf0a2ab056c0976951ac
clin <clin@splunk.com>
2025-03-14 21:30:23 -0700

    excel

--- hotel.py
+++ hotel.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import time
-import json
+import pandas
+import platform
 from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
 from selenium.webdriver.chrome.webdriver import Options
 from selenium.webdriver.common.keys import Keys
@@ -22,17 +23,20 @@ def parse_options():
 
 def main():
     options = parse_options()
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # 無頭模式 (Headless Mode)
-    chrome_options.add_argument("--no-sandbox")  # 避免權限問題
-    chrome_options.add_argument("--disable-dev-shm-usage")  # 避免共享內存不足問題
-    chrome_options.add_argument("--disable-gpu")  # 在無頭模式下需要禁用 GPU
-    chrome_options.add_argument("--disable-setuid-sandbox")  # 在容器內運行時必須禁用
-    chrome_options.add_argument("--window-size=1920,1080")
-    driver = Chrome(options=chrome_options)
+    if platform.system() != "Darwin":
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # 無頭模式 (Headless Mode)
+        chrome_options.add_argument("--no-sandbox")  # 避免權限問題
+        chrome_options.add_argument("--disable-dev-shm-usage")  # 避免共享內存不足問題
+        chrome_options.add_argument("--disable-gpu")  # 在無頭模式下需要禁用 GPU
+        chrome_options.add_argument("--disable-setuid-sandbox")  # 在容器內運行時必須禁用
+        chrome_options.add_argument("--window-size=1920,1080")
+        driver = Chrome(options=chrome_options)
+    else:
+        driver = Chrome()
+
     driver.get(f"https://www.google.com/travel/search?q={options.name}&hl=zh-Hant-CA")
-    with open("get.png", "wb") as png:
-        png.write(driver.get_screenshot_as_png())
+
     # time.sleep(10)
     print("Setting check-in date")
     checkin = driver.find_element(By.CSS_SELECTOR, '[placeholder="登機報到頁面"]')
@@ -47,22 +51,29 @@ def main():
         options.check_out + Keys.ENTER)
     time.sleep(5)
     # 查看更多選項
-    print("Getting prices")
-    with open("after_dates.png", "wb") as png:
-        png.write(driver.get_screenshot_as_png())
     driver.find_element(By.CSS_SELECTOR, '[jsname="wQivvd"]').click()
     time.sleep(5)
 
+    with open("prices.png", "wb") as png:
+        png.write(driver.get_screenshot_as_png())
+
     all_options = driver.find_elements(By.CSS_SELECTOR, '[jsname="Z186"]')[-1]
-    prices = {}
+    prices = []
     for row in all_options.find_elements(By.CSS_SELECTOR, '.ADs2Tc'):
         try:
             ota = row.find_element(By.CSS_SELECTOR, '[data-click-type="268"]').text
             price = row.find_element(By.CLASS_NAME, 'iqYCVb').text
-            prices[ota] = price
+            prices.append({"OTA": ota, "price": price})
         except NoSuchElementException:
             print(row.text)
     print(prices)
+
+    data = {options.name: prices}
+
+    with pandas.ExcelWriter("data.xlsx", engine="xlsxwriter") as writer:
+        for sheet_name, records in data.items():
+            df = pandas.DataFrame(records)
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
 
 
 if __name__ == "__main__":
