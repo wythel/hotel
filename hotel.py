b8dc7ef64b888fff9692000f7cbda5d9e4fded89
Wythe Lin <showtime.cs94@gmail.com>
2025-03-18 21:22:56 -0700

    Mar18bug

--- hotel.py
+++ hotel.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import time
 from typing import Dict, List
+from datetime import datetime, timedelta
 import pandas
 import platform
 from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
@@ -8,6 +9,11 @@ from selenium.webdriver.chrome.webdriver import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.remote.webelement import WebElement
+
+ONE_DAY = timedelta(days=1)
 
 
 def parse_options():
@@ -16,9 +22,9 @@ def parse_options():
     parser.add_argument(
         "--name", dest="name", help="Hotel name", required=False)
     parser.add_argument(
-        "--check-in", dest="check_in", help="Check in date", required=False)
+        "--start-date", dest="start", help="start date", required=False)
     parser.add_argument(
-        "--check-out", dest="check_out", help="Checkout date", required=False)
+        "--end-date", dest="end", help="end date", required=False)
     parser.add_argument(
         "--from-csv-file", dest="from_csv_file",
         help="Provide names and check in/out dates from a csv file",
@@ -27,10 +33,58 @@ def parse_options():
     return parser.parse_args()
 
 
+def get_screenshot(driver: Chrome, file_name: str) -> None:
+    """
+    get screenshot
+    """
+    with open(file_name, "wb") as png:
+        png.write(driver.get_screenshot_as_png())
+
+
+def get_price_from_row(row: WebElement) -> str:
+    """
+    get the hotel prices from a row
+    """
+    class_names = ('nDkDDb', 'iqYCVb', 'MW1oTb', 'UeIHqb')
+
+    for class_name in class_names:
+        try:
+            if row.find_element(By.CLASS_NAME, class_name).text:
+                return row.find_element(By.CLASS_NAME, class_name).text
+        except NoSuchElementException:
+            pass
+
+    return ''
+
+
+def open_all_options(driver: Chrome) -> None:
+    """
+    一直按查看更多選項，直到沒有為止
+    """
+    try:
+        driver.find_element(By.CSS_SELECTOR, '[jsname="wQivvd"]').click()
+    except NoSuchElementException:
+        pass
+    except ElementNotInteractableException:
+        pass
+    except StaleElementReferenceException:
+        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR, '[jsname="wQivvd"]').click()
+
+    while True:
+        try:
+            driver.find_element(By.CLASS_NAME, "bbRZy").click()
+        except ElementNotInteractableException:
+            return
+        except NoSuchElementException:
+            return
+
+
 def get_hotel_prices(name: str, checkin_date: str, checkout_date: str) -> List[Dict]:
     """
     get the hotel price by given name and check in/out date
     """
+    print(f"Getting prices of {name} between {checkin_date} and {checkout_date}")
 
     if platform.system() != "Darwin":
         chrome_options = Options()
@@ -45,34 +99,52 @@ def get_hotel_prices(name: str, checkin_date: str, checkout_date: str) -> List[D
         driver = Chrome()
 
     driver.get(f"https://www.google.com/travel/search?q={name}&hl=zh-Hant-CA")
+    time.sleep(3)
+    try:
+        driver.find_element(By.CSS_SELECTOR, f'[aria-label="{name}"]').click()
+        time.sleep(5)
+    except NoSuchElementException:
+        pass
+    try:
+        checkin = driver.find_element(By.CSS_SELECTOR, '[placeholder="登機報到頁面"]')
+        checkin.send_keys(
+            Keys.BACK_SPACE * len(checkin.get_attribute("value")) +
+            checkin_date + Keys.ENTER)
+        time.sleep(5)
+        checkout = driver.find_element(By.CSS_SELECTOR, '[placeholder="退房"]')
+        checkout.send_keys(
+            Keys.BACK_SPACE * len(checkout.get_attribute("value")) +
+            checkout_date + Keys.ENTER)
+        time.sleep(5)
+    except NoSuchElementException:
+        print(f"設定日期失敗: {name}-{checkin_date}-{checkout_date}")
+        get_screenshot(driver, f"設定日期失敗-{name}-{checkin_date}-{checkout_date}")
+        return []
+    except ElementNotInteractableException:
+        print(f"設定日期失敗: {name}-{checkin_date}-{checkout_date}")
+        get_screenshot(driver, f"設定日期失敗-{name}-{checkin_date}-{checkout_date}")
+        return []
 
-    print("Setting check-in date")
-    checkin = driver.find_element(By.CSS_SELECTOR, '[placeholder="登機報到頁面"]')
-    checkin.send_keys(
-        Keys.BACK_SPACE * len(checkin.get_attribute("value")) +
-        checkin_date + Keys.ENTER)
-    time.sleep(5)
-    print("Setting check-out date")
-    checkout = driver.find_element(By.CSS_SELECTOR, '[placeholder="退房"]')
-    checkout.send_keys(
-        Keys.BACK_SPACE * len(checkout.get_attribute("value")) +
-        checkout_date + Keys.ENTER)
-    time.sleep(5)
     # 查看更多選項
-    driver.find_element(By.CSS_SELECTOR, '[jsname="wQivvd"]').click()
-    time.sleep(5)
+    open_all_options(driver=driver)
 
-    file_name: str = f"{name}_{checkin_date}_{checkout_date}_prices.png"
-    with open(file_name, "wb") as png:
-        png.write(driver.get_screenshot_as_png())
-
-    all_options = driver.find_elements(By.CSS_SELECTOR, '[jsname="Z186"]')[-1]
+    file_name: str = f"{name}_{checkin_date}_{checkout_date}.png"
+    get_screenshot(driver, file_name)
+    try:
+        all_options = [
+            elem for elem in driver.find_elements(By.CSS_SELECTOR, '[jsname="Z186"]')
+            if elem.text != ''][-1]
+    except IndexError:
+        print(f"找不到{name}在{checkin_date}和{checkout_date}之間的價錢")
+        driver.quit()
+        return []
     prices = []
     for row in all_options.find_elements(By.CSS_SELECTOR, '.ADs2Tc'):
         try:
             ota = row.find_element(By.CSS_SELECTOR, '[data-click-type="268"]').text
-            price = row.find_element(By.CLASS_NAME, 'iqYCVb').text
-            prices.append({"OTA": ota, "price": price})
+            price = get_price_from_row(row)
+            if price:
+                prices.append({"OTA": ota, "price": price})
         except NoSuchElementException:
             print(row.text)
     driver.quit()
@@ -86,17 +158,25 @@ def main():
         data = {key: get_hotel_prices(
             options.name, options.check_in, options.check_out)}
     else:
-        data = {}
         reader = pandas.read_csv(options.from_csv_file)
         for row in reader.to_dict(orient="records"):
-            key = "_".join((row['name'], row['checkin'], row['checkout']))
-            data[key] = get_hotel_prices(
-                row['name'], row['checkin'], row['checkout'])
+            start = datetime.strptime(row['start'], "%m月%d日")
+            end = datetime.strptime(row['end'], "%m月%d日") + ONE_DAY
+            hotel = row['name']
+            print(f"Getting prices of {hotel} between {row['start']} and {row['end']}")
+            data = {}
+            while start < end:
+                checkin = datetime.strftime(start, "%m月%d日")
+                checkout = datetime.strftime(start + ONE_DAY, "%m月%d日")
+                key = "_".join((checkin, checkout))
+                data[key] = get_hotel_prices(
+                    row['name'], checkin, checkout)
+                start += ONE_DAY
 
-    with pandas.ExcelWriter("data.xlsx", engine="xlsxwriter") as writer:
-        for sheet_name, records in data.items():
-            df = pandas.DataFrame(records)
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
+            with pandas.ExcelWriter(f"{hotel}.xlsx", engine="xlsxwriter") as writer:
+                for sheet_name, records in data.items():
+                    df = pandas.DataFrame(records)
+                    df.to_excel(writer, sheet_name=sheet_name, index=False)
 
 
 if __name__ == "__main__":
